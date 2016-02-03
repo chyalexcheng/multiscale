494deb5490b7753375c468ab31060e33842db2f6
Hongyang Cheng <chyalexcheng@gmail.com>
2016-02-03 16:27:01 +0900

    Consider vertical surcharge in RVE

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # multiscale
+Multiscale modeling approach for soil-geosynthetic interaction
+
+# Escript functions
+msFEM2DExplicit.py
+msFEM2DExplicit.pyc
+
+# Yade functions
+simDEM.py
+simDEM.pyc
+
+# other functions
+saveGauss.py
+saveGauss.pyc
+
+# Yade script to build external scene
+msTest1_prepDEDomain.py
+msTest2_prepDEDomain.py
+# Yade script to build internal scene
+create DEM particle packing:	prepareAlumRods.py
+preare clean scenes:		prepareRVE.py 
+
+# main script
+msTest1_implicit.py
+msTest1_explicit.py
+msTest2_explicit.py
+
+# profiler
+msTest1.profile
+
+# basic initial packing
+alumRods0.txt
+
+# files to build external DE scene
+BxMsh1.npy	BxMsh2.npy	BxMsh3.npy	BxMsh4.npy
+BRefIDMsh1.npy	BRefIDMsh2.npy	BRefIDMsh3.npy	BRefIDMsh4.npy
+
+# msh files
+Msh1.geo
+Msh1.msh	Msh2.msh	Msh3.msh	Msh4.msh
--- prepareAlumRods.py
+++ prepareAlumRods.py
@@ -1,21 +1,31 @@
 from yade import pack
 
-O.materials.append(FrictMat(young=7.5e8,poisson=.016,frictionAngle=radians(0)))
+random = False
+pressure = -2.e4
+sceneName = 'Test2'
+spMat = O.materials.append(FrictMat(young=7.5e8,poisson=.016,frictionAngle=radians(16)))
 sp = pack.SpherePack()
-size = .24
-sp.makeCloud(minCorner=(0,0,.05),maxCorner=(size,size,.05),\
-			    psdSizes=[.003,.005,.01],\
-				 psdCumm =[.2  ,.85 , 1.],\
-				 num=400, periodic=True)
-sp.toSimulation()
-for b in O.bodies:
-	if b.shape.radius<=.003/2:
-		b.shape.radius=.003/2
-	elif .003/2<b.shape.radius<=.005/2:
-		b.shape.radius=.005/2
-	elif .005/2<b.shape.radius<=.01/2:
-		b.shape.radius=.01/2
-O.cell.hSize = Matrix3(size,0,0, 0,size,0, 0,0,.1)
+if random:
+	size = .24
+	O.materials[0].frictionAngle = radians(0)
+	sp.makeCloud(minCorner=(0,0,.05),maxCorner=(size,size,.05),\
+				    psdSizes=[.003,.005,.01],\
+					 psdCumm =[.2  ,.85 , 1.],\
+					 num=400, periodic=True)
+	sp.toSimulation(material=spMat)
+	for b in O.bodies:
+		if b.shape.radius<=.003/2:
+			b.shape.radius=.003/2
+		elif .003/2<b.shape.radius<=.005/2:
+			b.shape.radius=.005/2
+		elif .005/2<b.shape.radius<=.01/2:
+			b.shape.radius=.01/2
+	O.cell.hSize = Matrix3(size,0,0, 0,size,0, 0,0,.1)
+else:
+	sp.load('alumRods0.txt')
+	sp.toSimulation(material=spMat)
+	O.periodic=True
+	O.cell.hSize = Matrix3(0.11280356495985018,0,0, 0,0.10837027507783764,0, 0,0,0.1)
 print len(O.bodies)
 
 for p in O.bodies:
@@ -37,12 +47,13 @@ O.engines = [
    ),
    PeriTriaxController(
       dynCell=True,
-      goal=(-1.e1,-1.e1,0),
-      stressMask=3,
+      # change goal and stress for altering DC
+      goal=(0,pressure,0),
+      stressMask=2,
       relStressTol=.001,
       absStressTol=1.,
       maxUnbalanced=.001,
-      maxStrainRate=(.1,.1,.0),
+      maxStrainRate=(.01,.001,.0),
       doneHook='term()',
       label='biax'
    ),
@@ -61,7 +72,9 @@ def term():
       p.state.angVel = Vector3.Zero
       p.state.refPos = p.state.pos
       p.state.refOri = p.state.ori
-   O.save('DE_alum.yade.gz')
+   O.save('./DE_exts/'+sceneName+'/DE_alum.yade.gz')
+   sp.fromSimulation()
+   sp.save('./DE_exts/'+sceneName+'/DE_alum.txt')
    O.pause()
 
 O.run()
